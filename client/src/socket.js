a52888fec9de64716f2ded038f5cf46dd50eb7ab
Kushagra Agarwal <kushagrawal766@gmail.com>
2026-09-12 23:34:40 +0530

    Use environment-based API/socket URLs for deployment

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000', {
-  autoConnect: false, // we connect manually once we know the user is logged in
+const socket = io(import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000', {
+  autoConnect: false,
 });
 
 export default socket;
